2d991d1be5162f5dea3d480455c2f07434ddff24
pavalex <Alex.P79@mail.ru>
2022-05-10 01:02:31 +0800

    fixed input-debounce

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const inputTxt = document.querySelector('.input-txt');
 const info = document.querySelector('.info');
 const info2 = document.querySelector('.info2');
 
-function debounce( callback, delay ) {
+const debounce = (callback, delay) => {
     let timeout;
     return function() {
         clearTimeout( timeout );
         timeout = setTimeout( callback, delay );
-    }
-}
+    };
+};
 
 const outputText = () => {
     info.textContent = inputTxt.value;
-}
+};
 
 const outputText2 = () => {
     info2.textContent = inputTxt.value;
-}
+};
 
 inputTxt.addEventListener('input', debounce(outputText, 1000));
 inputTxt.addEventListener('input', debounce(outputText2, 2000));
